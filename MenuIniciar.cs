a0f4fece922968869a99e0017b30284e5c2608ca
Bruno Barreiros Alves <124444173+BrunoBarreirosJogosDigitais@users.noreply.github.com>
2025-08-31 19:25:06 -0300

    Add files via upload

---
+++ MenuIniciar.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuIniciar : MonoBehaviour
+{
+    [Header("Painéis do Menu")] //Aqui vai mostar os paineis no menu iniciar sme precisar ir 
+    //                            para outra cena, voces veirifam ai que ainda falta eu 
+    //                            implementar o retorno para o menu normal. Ass. Bruno Barreiros
+    public GameObject painelSair;
+    public GameObject painelCreditos;
+    public GameObject painelConfiguracao;
+
+    public void Menu()
+    {
+        print("Menu");
+    }
+    public void IniciarJogo()
+    {
+        SceneManager.LoadScene("Iniciar");
+        print("Iniciar");
+    }
+    public void Creditos()
+    {
+        FecharTodosPaineis();
+        painelCreditos.SetActive(true);
+        print("Creditos");
+    }
+    public void Configuracao()
+    {
+        FecharTodosPaineis();
+        painelConfiguracao.SetActive(true);
+        print("Configuração");
+    }
+    // Aqui vai abrir o painel de confirmacao de saida
+    public void Sair()
+    {
+        FecharTodosPaineis();
+        painelSair.SetActive(true);
+        print("Deseja sair?");
+    }
+    // AQui tem o Botao "Sim" do painel
+    public void ConfirmarSair()
+    {
+        Application.Quit();
+        print("Saiu do jogo");
+    }
+    // Aqui tem o Botao "Nao" do painel pra cancelar a saida
+    public void CancelarSair()
+    {
+        painelSair.SetActive(false);
+        print("Cancelou saída");
+    }
+    void FecharTodosPaineis()
+    {
+        painelSair.SetActive(false);
+        painelCreditos.SetActive(false);
+        painelConfiguracao.SetActive(false);
+    }
+}
